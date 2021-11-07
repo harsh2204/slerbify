47a14fd7f2b3a5cc2a75f17fa376205bebaabe24
Harsh Gupta <harshgupta2204@live.com>
2021-11-07 01:24:41 -0500

    update

--- slerbify.py
+++ slerbify.py
@@ -2,6 +2,8 @@
 import time, math, random, string
 import requests
 import argparse
+#import asyncio
+from multiprocessing import Pool, TimeoutError
 
 HOSTNAME = 'https://slowedreverb.com'
 
@@ -39,17 +41,48 @@ def slerbify(file, slow, reverb):
     f = send_file(file, fields)
 
     with requests.get(HOSTNAME+f) as res:
-        outfilename = f'{file[:-4]}-slerbified.mp3'
-        with open(outfilename, 'wb') as outf:
-            outf.write(res.content)
-        print('Saved slerbified file as', outfilename)
+        if res.ok:
+            outfilename = f'{file[:-4]}-slrb{slow}{reverb}.mp3'
+            with open(outfilename, 'wb') as outf:
+                outf.write(res.content)
+            print('Saved slerbified file as', outfilename)
+        else:
+            raise Exception("{} - {}".format(res.status_code, res.text))
+
+
+#def process_parallel(b):
+#    # We sleep b/w requests to prevent rate limit restrictions
+#    #loop = asyncio.get_event_loop()
+#    with Pool(processes=4) as pool:
+#        
+#        files = b.files
+#        for s in files:
+#            #loop.run_in_executor(None, slerbify, s, b.slow, b.reverb)
+#            slerbify(s, b.slow, b.reverb)
+#            if args.sleep:
+#                print(f'Sleeping for {args.sleep} second(s)')
+#                time.sleep(args.sleep)
 
 
 def process_batch(b):
-    # We sleep b/w requests to prevent rate limit restrictions
-    files = b.files
-    for s in files:
-        slerbify(s, b.slow, b.reverb)
+    N = len(b.files)
+    if b.slow:
+        alpha= b.slow
+        beta = 25
+        slows = [max(min(int(random.weibullvariate(alpha, beta)), 100),75) for _ in range(N)]
+        #slows.sort()
+        print(slows)
+    if b.reverb:
+        alpha= 100
+        beta = b.reverb/100
+        reverbs = [max(min(int(random.gammavariate(alpha, beta)), 100), 0) for _ in range(N)]
+        #reverbs.sort()
+        print(reverbs)
+
+    
+    for i, s in enumerate(b.files):
+        slerbify(s, slows[i], reverbs[i])
+        # We sleep b/w requests to prevent rate limit restrictions
         if args.sleep:
             print(f'Sleeping for {args.sleep} second(s)')
             time.sleep(args.sleep)
@@ -58,9 +91,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Slerbify(slow + reverb) mp3 files using slowedreverb.com')
     parser.add_argument('-s', dest='slow', type=int, choices=range(75, 101), metavar='[75-100]', default=87, help='The slowness speed. (default: %(default)s)')
     parser.add_argument('-r', dest='reverb', type=int, choices=range(0, 101), metavar='[0-100]', default=80, help='The reverb value. (default: %(default)s)')
+    parser.add_argument('-p', '--parallel', dest='parallel', action='store_true', help='Enable parallelism')
     parser.add_argument('-d', '--delay', dest='sleep', type=int, default=2, help=argparse.SUPPRESS)
     parser.add_argument(dest='files', metavar='mp3_file', nargs='+', help='Any mp3 file(s) to be slerbified')
     args = parser.parse_args()
-    process_batch(args)
+    if args.parallel:
+        process_parallel(args)
+    else:
+        process_batch(args)
     exit(0)
     slerbify(args)
